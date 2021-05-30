ddf4df6fa6f2e2c87d3eec66a942df4ff145d007
matthias pineau <pineau.matthias@gmail.com>
2021-05-30 15:17:16 +0200

    add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,38 @@
 <template>
-  <div>
-    <div class="container">
-      <div class="home">
+  <div class="home">
+    <header class="home-header" :style="{ backgroundImage: 'url(' + require('@/assets/u.svg') + ')' }">
+      <div class="container">
+        
+        <h1 class="home-header__title h1">
+          La réalisation de vos projets 3D devient simple et rapide.
+        </h1>
 
-        <h1 class="h1">La réalisation de vos projets 3D devient simple et rapide.</h1>
-
-        <div class="category">
-          <div class="category__item">
+        <div class="home-header-content">
+          <div class="home-header-card">
             <h4 class="h4">Impression 3D</h4>
-            <p class="t1">Démarrer votre impression 3D en quelques clics. Nous vous guiderons pas à pas dans la conception. <br>En savoir plus</p>
-            <router-link to="/print-home" class="btn btn--secondary mt--20">Déposer un fichier</router-link>
+            <p class="t1">
+              Démarrer votre impression 3D en quelques clics. Nous vous
+              guiderons pas à pas dans la conception. <br />En savoir plus
+            </p>
+            <router-link to="/print-home" class="btn btn--secondary mt--20"
+              >Déposer un fichier</router-link
+            >
           </div>
 
-          <div class="category__item">
+          <div class="home-header-card">
             <h4 class="h4">Projet 3D</h4>
-            <p class="t1">Démarrer votre impression 3D en quelques clics. Nous vous guiderons pas à pas dans la conception. <br>En savoir plus</p>
-            <router-link to="/projet-home" class="btn btn--secondary mt--20">Démarrer un projet</router-link>
+            <p class="t1">
+              Démarrer votre impression 3D en quelques clics. Nous vous
+              guiderons pas à pas dans la conception. <br />En savoir plus
+            </p>
+            <router-link to="/projet-home" class="btn btn--secondary mt--20"
+              >Démarrer un projet</router-link
+            >
           </div>
         </div>
-
       </div>
-    </div>
+    </header>
+
   </div>
 </template>
 
